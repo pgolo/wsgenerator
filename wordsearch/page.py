d76f40917b285b3a7c35349152361cec519addfd
pgolo <p.golovatenko@gmail.com>
2020-12-17 22:05:21 -0500

    added legend to the page

--- wordsearch/page.py
+++ wordsearch/page.py
@@ -8,13 +8,29 @@ def css_style():
     return style
 
 def create(height, width, words):
+    # generate puzzle
     puzzle, hints = wordsearch.pretty_puzzle(height, width, words)
+
+    # elements
     html = et.Element('html')
     head = et.Element('head')
     body = et.Element('body')
     style = et.Element('style')
     style.text = css_style()
-    table = et.Element('table')
+    puzzle_caption = et.Element('h1')
+    puzzle_table = et.Element('table')
+    legend_caption = et.Element('h1')
+    legend_table = et.Element('table')
+
+    # add the content
+
+    # puzzle caption
+    puzzle_caption.text = 'Puzzle'
+
+    # legend caption
+    legend_caption.text = 'Legend'
+
+    # puzzle
     header_row = True
     for row in puzzle:
         html_row = et.Element('tr')
@@ -24,15 +40,40 @@ def create(height, width, words):
             html_cell.text = cell
             html_row.append(html_cell)
             header_column = False
-        table.append(html_row)
+        puzzle_table.append(html_row)
         header_row = False
+    
+    # legend
+    legend = [[]]
+    legend_columns = 3
+    n = 0
+    for word in hints:
+        legend[-1].append(word)
+        n += 1
+        if n % legend_columns == 0:
+            legend.append([])
+    if len(legend[-1]) == 0:
+        legend = legend[:-1]
+    legend[-1] += [''] * (len(legend[0]) - len(legend[-1]))
+    for row in legend:
+        html_row = et.Element('tr')
+        for cell in row:
+            html_cell = et.Element('td')
+            html_cell.text = cell
+            html_row.append(html_cell)
+        legend_table.append(html_row)
+    
+    # render all
     head.append(style)
-    body.append(table)
+    body.append(puzzle_caption)
+    body.append(puzzle_table)
+    body.append(legend_caption)
+    body.append(legend_table)
     html.append(head)
     html.append(body)
     html = et.tostring(html)
     return html
 
-result = create(4, 4, ['cat', 'dog', 'frog'])
-with open('out.html', mode='wb') as f:
+result = create(4, 4, ['cat', 'dog', 'frog', 'abc', 'def'])
+with open('puzzle.html', mode='wb') as f:
     f.write(result)
