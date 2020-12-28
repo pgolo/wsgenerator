d5c761e3d5dcbb2ee3d5966947f9fa1857e2c607
pgolo <p.golovatenko@gmail.com>
2020-12-27 22:44:43 -0500

    size 15x15

--- wordsearch/page.py
+++ wordsearch/page.py
@@ -10,6 +10,6 @@ def create(height, width, words):
         puzzle_code = f.read().replace('$words', str(wordbank).replace('\'', '"')).replace('$puzzle', str(puzzle).replace('\'', '"'))
     return puzzle_code
 
-puzzle = create(11, 11, ['Maryland', 'Virginia', 'Vermont', 'Montana', 'Delaware', 'Alabama', 'Alaska', 'Hawaii', 'Arizona', 'California', 'Texas', 'Indiana'])
+puzzle = create(15, 15, ['Maryland', 'Virginia', 'Vermont', 'Montana', 'Delaware', 'Alabama', 'Alaska', 'Hawaii', 'Arizona', 'California', 'Texas', 'Indiana'])
 with open('puzzle.html', mode='w', encoding='utf8') as f:
     f.write(puzzle)
