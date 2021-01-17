7b7de30b4247004cccf20e7b37a101c29c32cf02
pgolo <p.golovatenko@gmail.com>
2021-01-17 01:51:25 -0500

    web application

--- wordsearch/example.py
+++ wordsearch/example.py
@@ -1,48 +1,31 @@
+import random
 import wordsearch
 
-words = [
-    'cow',
-    'goat',
-    'pig',
-    'buffalo',
-    'chicken',
-    'sheep',
-    'lamb',
-    'goose',
-    'turkey',
-    'duck'
-    'horse',
-    'cattle',
-    'llama',
-    'bison',
-    'hen',
-    'calf',
-    'rooster',
-    'bull',
-    'donkey',
-    'dog'
-    'geese',
-    'fish',
-    'deer',
-    'birds',
-    'bees',
-    'qwerty',
-    'uiop',
-    'asdfg',
-    'zxcvxc',
-    'topyuytsdgfwqqq'
-]
+def create(*args, **kwargs):
+    puzzle, hints = wordsearch.pretty_puzzle(*args, **kwargs)
+    wordbank = list(hints.keys())
+    random.shuffle(wordbank)
+    puzzle_code = ''
+    with open('wordsearch/templates/puzzle.html', mode='r', encoding='utf8') as f:
+        puzzle_code = f.read().replace('{{ url_for(\'static\', filename=\'game.js\') }}', 'wordsearch/static/game.js').replace('{{words|safe}}', str(wordbank).replace('\'', '"')).replace('{{puzzle|safe}}', str(puzzle).replace('\'', '"')).replace('{{solution|safe}}', str(hints).replace('\'', '"'))
+    return puzzle_code
 
-# words = [
-#     'e',
-#     'aaa',
-#     'bb',
-#     'c',
-#     'dd'
-# ]
 
-puzzle, hints = wordsearch.pretty_puzzle(15, 15, words)
-for row in puzzle:
-    print(row)
-for word in hints:
-    print(word, hints[word])
+z = '''
+###
+  #
+  #
+  #
+  #
+   #
+    #
+     #
+     #
+     #
+   ###
+'''
+#puzzle = create('aaa', 'bbb', 'ccc', words=['ddd', 'eee'], template=z)
+puzzle = create(height=15, width=15, words=['Maryland', 'Virginia', 'Vermont', 'Montana', 'Delaware', 'Alabama', 'Alaska', 'Hawaii', 'Arizona', 'California', 'Texas', 'Indiana', 'Illinois', 'Minnesota', 'Wisconsin', 'Ohio', 'Nebraska', 'Iowa', 'Kentucky', 'Kansas', 'Florida', 'Arkansas'])
+with open('example.html', mode='w', encoding='utf8') as f:
+    f.write(puzzle)
+
--- wordsearch/webapp.py
+++ wordsearch/webapp.py
@@ -1,38 +1,44 @@
 from flask import Flask, request, Response, render_template, jsonify, json
 import random
 import wordsearch
-import page
 
 app = Flask(__name__)
 
 def get_simple_args(rqst):
-    return json.loads(request.args['height']), json.loads(request.args['width']), json.loads(request.args['words']).split(' ')
+    return json.loads(rqst.args['height']), json.loads(rqst.args['width']), json.loads(rqst.args['words']).split(' ')
+
+def get_fancy_args(rqst):
+    data = rqst.form
+    return data['template'], data['words'].split(' ')
+
+def generate_puzzle(**kwargs):
+    if 'template' in kwargs:
+        assert 'words' in kwargs
+        puzzle, solution = wordsearch.pretty_puzzle(template=kwargs['template'], words=kwargs['words'])
+    else:
+        assert 'height' in kwargs
+        assert 'width' in kwargs
+        assert 'words' in kwargs
+        puzzle, solution = wordsearch.pretty_puzzle(height=kwargs['height'], width=kwargs['width'], words=kwargs['words'])
+    wordbank = list(solution.keys())
+    random.shuffle(wordbank)
+    out = {'words': wordbank, 'puzzle': puzzle, 'solution': solution}
+    return out, wordbank, puzzle, solution
 
 @app.route('/api/instant/simple', methods=['GET'])
 def get_simple_puzzle():
     height, width, words = get_simple_args(request)
-    puzzle, solution = wordsearch.pretty_puzzle(height=height, width=width, words=words)
-    wordbank = list(solution.keys())
-    random.shuffle(wordbank)
-    out = {'words': wordbank, 'puzzle': puzzle, 'solution': solution}
+    out, wordbank, puzzle, solution = generate_puzzle(height=height, width=width, words=words)
+    if 'page' in request.args:
+        return render_template('puzzle.html', words=wordbank, puzzle=puzzle, solution=solution)
     return jsonify(out)
 
-@app.route('/api/instant/fancy', methods=['POST'])
+@app.route('/api/instant/fancy', methods=['POST', 'GET'])
 def get_fancy_puzzle():
-    data = request.form
-    template = data['template']
-    words = data['words'].split(' ')
-    puzzle, solution = wordsearch.pretty_puzzle(template=template, words=words)
-    wordbank = list(solution.keys())
-    random.shuffle(wordbank)
-    out = {'words': wordbank, 'puzzle': puzzle, 'solution': solution}
-    return jsonify(out)
-
-@app.route('/api/instant/simple/page', methods=['GET'])
-def get_simple_page():
-    height, width, words = get_simple_args(request)
-    html = page.create(height=height, width=width, words=words)
-    out = {'html': html}
+    template, words = get_fancy_args(request)
+    out, wordbank, puzzle, solution = generate_puzzle(template=template, words=words)
+    if 'page' in request.args:
+        return render_template('puzzle.html', words=wordbank, puzzle=puzzle, solution=solution)
     return jsonify(out)
 
 if __name__ == '__main__':
